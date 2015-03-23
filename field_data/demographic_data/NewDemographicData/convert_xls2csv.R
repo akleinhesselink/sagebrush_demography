22f5aaca0174c2b2eb8728809d77f60530422232
Andy <andy.kleinhesselink@gmail.com>
2015-03-23 13:07:19 -0600

    adding a file conversion utility to convert xlsx to csv for data.  This will allow me to put the csv files in version control

---
+++ field_data/demographic_data/NewDemographicData/convert_xls2csv.R
@@ -0,0 +1,26 @@
+#!usr/bin/Rscript
+#### script for batch conversion of XLS and XLSX file to csv 
+#### Run from the command line to convert all xlsx files in the current
+#### working directory to csv files with the same name 
+
+require( xlsx)
+
+XLSXpattern = '*\\.xlsx$'
+
+wd = system(command='pwd', intern = TRUE)
+setwd(wd)
+
+fileList = dir(path = '.', pattern= XLSXpattern)
+
+startTime = Sys.time()
+count = 0
+
+for (f in fileList) { 
+  print(paste('converting ', f))
+  tempDat = read.xlsx( f ,sheetIndex= 1, startRow=1)
+  write.table(x= tempDat, file = gsub(f, pattern= XLSXpattern, replacement = '.csv'), row.names = FALSE, sep = ',')
+  count = count + 1 
+}
+
+print( paste( count, 'files converted in', Sys.time() - startTime) )
+
