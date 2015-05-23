601b60e406d8e9e823d08f3ed2a983d500dea77a
Andy <andy.kleinhesselink@gmail.com>
2015-05-23 13:07:27 -0600

    Adding a make sageDB bash script that runs through all the necessary Rscripts to make the sage demography database.  Removing the R script that it replaces.

---
+++ field_data/demographic_data/NewDemographicData/MakeSageDB.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Rscript reCreateSageDB.R
+Rscript summer2013StatusUpdate.R
+Rscript fall2013Update.R
+Rscript earlySpring2014Update.R
+Rscript spring2014FallTransplantsUpdate.R
+Rscript spring2014Update.R
+Rscript Fall2014Update.R
+Rscript makeFlowersTable.R
+Rscript UpdateFlowersTable.R
