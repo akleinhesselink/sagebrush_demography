c90c5eb9f857e8e61067a53cb2d9afa96dabbf45
Andy <andy.kleinhesselink@gmail.com>
2015-07-02 16:49:22 -0600

    adding active and missing checks to the spring update script, lots of plants were recorded in the spring that were previously recorded as dead

--- field_data/demographic_data/NewDemographicData/earlySpring2014Update.R
+++ field_data/demographic_data/NewDemographicData/earlySpring2014Update.R
@@ -28,7 +28,12 @@ earlySpringUpdate = earlySpringUpdate[ !is.na(earlySpringUpdate$status),  ] ### 
 
 earlySpringUpdate[ , c('ch', 'c1', 'c2', 'canopy', 'stem_d1', 'stem_d2', 'infls')] <- as.numeric( unlist( earlySpringUpdate[ , c('ch', 'c1', 'c2', 'canopy', 'stem_d1', 'stem_d2', 'infls')] ))
 
-db = dbConnect(SQLite(), dbname = '../sage.sqlite')
+db = dbConnect(SQLite(), dbname = 'sage.sqlite')
+
+lastDate = max(earlySpringUpdate$date)
+res = dbSendQuery( db, "SELECT * FROM plants WHERE active = 1 AND date( start_date) <= date( ? )" , list( lastDate))
+active = fetch( res, -1)
+dbClearResult( res )
 
 #### run checks 
 see_if( checkStatus (earlySpringUpdate$status))
@@ -46,10 +51,32 @@ see_if( checkPositiveRange( earlySpringUpdate$canopy, upper.limit = 150))
 see_if( checkPositiveRange( earlySpringUpdate$infls, upper.limit = 900))
 see_if( checkAllMonths( earlySpringUpdate$date[ which( earlySpringUpdate$infls > 0 )], early= 9, late = 11))
 
+checkActive( earlySpringUpdate$ID, active$ID)
+
+bad = c(10, 271, 634, 639, 441, 642, 442, 646, 448, 449, 288, 410, 411, 717, 718, 721, 724)
+earlySpringStatus[ earlySpringStatus$ID %in% bad , ] #### as long as the spring status is 0 it should be ok, just confirming ones that died in the fall
+
+missing = checkForMissing( earlySpringUpdate$ID, active$ID ) 
+
+missingInfo = list()
+
+for(i in 1:length(missing)) { 
+  res = dbSendQuery( db, 'SELECT * FROM plants WHERE ID == ?', list(missing[i]))
+  missingInfo[[i]] = fetch(res, -1)
+  dbClearResult(res) 
+}
+
+do.call(rbind, unique( missingInfo )) ##### missing the class 5s, 
+                                      ##### 771 is a new seedling is a class 6 and needs to be added to the datasheet 
+                                      ###### watch for 678 a class 4 in later updates 
+
+newSeedling = earlySpringUpdate[ earlySpringUpdate$field_tag == 771, ]
+newSeedling$ID = 1087
+newSeedling$status = 1 
+earlySpringUpdate = rbind( earlySpringUpdate, newSeedling)
 
 dbWriteTable(db, name = 'status', value = earlySpringUpdate, 
              append = TRUE, row.names = FALSE)
-
 
 res = dbSendQuery( db, "SELECT ID, field_tag, date, status FROM status WHERE date(date) > date('2013-09-01') 
                    AND date(date) < date('2014-01-01')")
